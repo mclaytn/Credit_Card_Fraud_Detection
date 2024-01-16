07002b6a6189e8ffa2412536c4cf5f6aa09bfe9a
Mary Clayton <152268652+mclaytn@users.noreply.github.com>
2024-01-15 23:32:52 -0700

    Update Credit_Card_AI.py

--- Credit_Card_AI.py
+++ Credit_Card_AI.py
@@ -1,6 +1,8 @@
 # Imported Libraries
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 # from sklearn.linear_model import LinearRegression, LogisticRegression
 
 # Read dataset
@@ -9,11 +11,14 @@ credit_df = pd.read_csv('fraudTest.csv')
 # Prepping Dataset
 def prep_data(credit_df: pd.DataFrame) -> (np.ndarray):
     x = credit_df.iloc[:, 2:30].values
-    y = credit_df.Class.values
+    y = credit_df.__class__.values
     return x, y
 
 # Plotting Dataset
-    
-
-
-
+def plot_data(x: np.ndarray, y: np.ndarray):
+      plt.scatter(x[y == 0, 0], x[y == 0, 1], label="Exhibit #0", alpha=0.5, linewidth=0.15)
+      plt.scatter(x[y == 1, 0], x[y == 1, 1], label="Exhibit #1", alpha=0.5, linewidth=0.15, c='r')
+      plt.legend()
+      return plt.show()
+x, y = prep_data(credit_df)
+plot_data(x, y)
